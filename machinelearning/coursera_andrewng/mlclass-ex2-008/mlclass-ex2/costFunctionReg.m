220aa94fad59e009c25ad9bb1e3411b3a25cad53
Neeraj Goel <neeraj.goel82@gmail.com>
2015-03-03 02:22:03 +0530

    implementing the cost function without reglarization for the regularization problem

--- machinelearning/coursera_andrewng/mlclass-ex2-008/mlclass-ex2/costFunctionReg.m
+++ machinelearning/coursera_andrewng/mlclass-ex2-008/mlclass-ex2/costFunctionReg.m
@@ -18,7 +18,24 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+%Generating the cost of the hypothesis function
+hThetaX = sigmoid(X*theta);
+costMatrix = -(y .* log(hThetaX)) - ((1 - y) .* log( 1 - hThetaX));
+delta = sum (costMatrix);
+%regularizationParam = (lambda/(2*m))*sum(theta.^2) - theta(1);
+%J = (1 / m ) * delta + regularizationParam;
+J = (1 / m ) * delta;
 
+%Generating gradient
+%This will generate vector for errors based on theta. It will generate a m * 1 matrix
+diffVector = hThetaX - y;
+
+%This is the variable to store the diff vector transpose multiplied by X. This will generate 1 * n matrix.
+diffVectorX = diffVector' * X;
+
+ %Transpose for the above generated vector. This will generate n * 1 matrix
+diffVectorXTranspose = diffVectorX';
+grad = (diffVectorXTranspose./m);
 
 
 
