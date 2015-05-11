46d4bf18d157100787c91b5387736bd52cb0f6ef
Neeraj Goel <neeraj.goel82@gmail.com>
2015-05-11 21:33:18 +0530

    now we are creating the tsne visualization for the digits

--- machinelearning/tsne/tsnevisualize.py
+++ machinelearning/tsne/tsnevisualize.py
@@ -44,3 +44,52 @@ digits.data.shape
 print(digits['DESCR'])
 
 
+nrows, ncols = 2, 5
+plt.figure(figsize=(6,3))
+plt.gray()
+for i in range(ncols * nrows):
+    ax = plt.subplot(nrows, ncols, i + 1)
+    ax.matshow(digits.images[i,...])
+    plt.xticks([]); plt.yticks([])
+    plt.title(digits.target[i])
+plt.savefig('images/digits-generated.png', dpi=150)
+
+# We first reorder the data points according to the handwritten numbers.
+X = np.vstack([digits.data[digits.target==i]
+               for i in range(10)])
+y = np.hstack([digits.target[digits.target==i]
+               for i in range(10)])
+
+digits_proj = TSNE(random_state=RS).fit_transform(X)
+
+
+def scatter(x, colors):
+    # We choose a color palette with seaborn.
+    palette = np.array(sns.color_palette("hls", 10))
+
+    # We create a scatter plot.
+    f = plt.figure(figsize=(8, 8))
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(x[:,0], x[:,1], lw=0, s=40,
+                    c=palette[colors.astype(np.int)])
+    plt.xlim(-25, 25)
+    plt.ylim(-25, 25)
+    ax.axis('off')
+    ax.axis('tight')
+
+    # We add the labels for each digit.
+    txts = []
+    for i in range(10):
+        # Position of each label.
+        xtext, ytext = np.median(x[colors == i, :], axis=0)
+        txt = ax.text(xtext, ytext, str(i), fontsize=24)
+        txt.set_path_effects([
+            PathEffects.Stroke(linewidth=5, foreground="w"),
+            PathEffects.Normal()])
+        txts.append(txt)
+
+    return f, ax, sc, txts
+
+
+scatter(digits_proj, y)
+plt.savefig('images/digits_tsne-generated.png', dpi=120)
