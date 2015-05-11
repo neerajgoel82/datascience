70ac91731e8ee0e1a4badfbf036ab5052878e0f3
Neeraj Goel <neeraj.goel82@gmail.com>
2015-05-11 20:44:05 +0530

    now we are successfully loading the image dataset

--- machinelearning/tsne/tsnevisualize.py
+++ machinelearning/tsne/tsnevisualize.py
@@ -1,1 +1,46 @@
 __author__ = 'neeraj'
+
+
+# That's an impressive list of imports.
+import numpy as np
+from numpy import linalg
+from numpy.linalg import norm
+from scipy.spatial.distance import squareform, pdist
+
+# We import sklearn.
+import sklearn
+from sklearn.manifold import TSNE
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import scale
+
+# We'll hack a bit with the t-SNE code in sklearn 0.15.2.
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.manifold.t_sne import (_joint_probabilities,
+                                    _kl_divergence)
+from sklearn.utils.extmath import _ravel
+# Random state.
+RS = 20150101
+
+# We'll use matplotlib for graphics.
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
+import matplotlib
+#%matplotlib inline
+
+# We import seaborn to make nice plots.
+import seaborn as sns
+sns.set_style('darkgrid')
+sns.set_palette('muted')
+sns.set_context("notebook", font_scale=1.5,
+                rc={"lines.linewidth": 2.5})
+
+# We'll generate an animation with matplotlib and moviepy.
+from moviepy.video.io.bindings import mplfig_to_npimage
+import moviepy.editor as mpy
+
+digits = load_digits()
+digits.data.shape
+
+print(digits['DESCR'])
+
+
