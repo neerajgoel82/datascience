4e92b7814891589abcba1d130e4647fb5bd2fbc4
Neeraj Goel <negoel@adobe.com>
2015-05-14 15:21:09 +0530

    refactoring code structure

--- nlp/sentimentanalysis/tweetclassifier.py
+++ nlp/sentimentanalysis/tweetclassifier.py
@@ -1,6 +1,8 @@
 __author__ = 'neeraj'
 
 from textblob.classifiers import NaiveBayesClassifier
+from textblob import TextBlob
+from nltk.corpus import movie_reviews
 
 train = [
     ('I love this sandwich.', 'pos'),
@@ -27,3 +29,16 @@ cl = NaiveBayesClassifier(train)
 
 print cl.classify("Their burgers are amazing")  # "pos"
 print cl.classify("I don't like their pizza.")  # "neg"
+
+blob = TextBlob("The beer was amazing. "
+                "But the hangover was horrible. My boss was not happy.",
+                classifier=cl)
+
+print blob.classify()  # "neg"
+for sentence in blob.sentences:
+    print(sentence)
+    print(sentence.classify())
+
+print cl.accuracy(test)
+
+cl.show_informative_features(50)
