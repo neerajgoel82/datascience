223230b284f2ddb670711c97784ecb1f7c8924f5
Neeraj Goel <negoel@adobe.com>
2016-01-07 19:57:19 +0530

    now we are able to send the arguments as well to the spark job

--- spark/SimpleApp/src/main/java/com/adobe/SimpleApp/SimpleApp.java
+++ spark/SimpleApp/src/main/java/com/adobe/SimpleApp/SimpleApp.java
@@ -1,11 +1,39 @@
 package com.adobe.SimpleApp;
 import org.apache.spark.api.java.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
 
 public class SimpleApp {
+
+
   public static void main(String[] args) {
-    String logFile = "/home/neeraj/Softwares/spark-1.6.0/README.md"; // Should be some file on your system
+	  Options options = new Options();
+	  options.addOption("f", false, "Enter file path");
+	  String logFile = args[0];//"/home/neeraj/Softwares/spark-1.6.0/README.md"; // Should be some file on your system
+	  
+	  
+	  
+	  CommandLineParser parser = null ;
+	  CommandLine cmd = null ; 
+	 
+	  parser = new PosixParser();
+	  try {
+		cmd = parser.parse( options, args);
+		if(cmd.hasOption("f")) 
+		{
+		  logFile = cmd.getOptionValue("f");
+		}
+	} catch (ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  
+	      
     SparkConf conf = new SparkConf().setAppName("Simple Application");
     JavaSparkContext sc = new JavaSparkContext(conf);
     JavaRDD<String> logData = sc.textFile(logFile).cache();
